6061e342e0b2938b94ddd3d552716e06c9018b3c
orlovNikitq <130779328+orlovNikitq@users.noreply.github.com>
2024-03-10 19:21:28 +0300

    Add files via upload

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel_color = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.panel_color);
+            this.groupBox1.Location = new System.Drawing.Point(12, 353);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(659, 114);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Инструменты";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(201, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 38);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(146, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 38);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(91, 63);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(251, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 1;
+            // 
+            // panel_color
+            // 
+            this.panel_color.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel_color.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_color.Location = new System.Drawing.Point(6, 63);
+            this.panel_color.Name = "panel_color";
+            this.panel_color.Size = new System.Drawing.Size(53, 45);
+            this.panel_color.TabIndex = 0;
+            this.panel_color.DoubleClick += new System.EventHandler(this.panel_color_DoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(659, 302);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(91, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 38);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(256, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 48);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(311, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 48);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(366, 19);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 48);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(422, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 48);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 474);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel_color;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        SolidBrush mybrush;
+        Graphics my_g;
+        bool isDrawing = false;
+        Point startPoint;
+        bool flagRec,flagEcl,filling, line = false; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mybrush = new SolidBrush(panel_color.BackColor);
+            my_g = panel1.CreateGraphics();
+        }
+
+        private void panel_color_DoubleClick(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                panel_color.BackColor = colorDialog1.Color;
+                mybrush.Color = colorDialog1.Color;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            startPoint = e.Location;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+
+            if (flagRec == true)
+            {
+                mybrush.Color = panel_color.BackColor;
+                if (filling == true)
+                {
+                    my_g.FillRectangle(mybrush, startPoint.X, startPoint.Y, e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
+                }
+                else
+                {
+                    Pen myPen = new Pen(panel_color.BackColor);
+                    myPen.Width = trackBar1.Value;
+                    if (flagRec == true)
+                        my_g.DrawRectangle(myPen, startPoint.X, startPoint.Y, e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
+
+                    myPen.Dispose();
+                }
+            }
+            else if (flagEcl == true)
+            {
+                mybrush.Color = panel_color.BackColor;
+                if (filling == true)
+                {
+                    my_g.FillEllipse(mybrush, startPoint.X, startPoint.Y, e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
+                }
+                else
+                {
+                    Pen myPen = new Pen(panel_color.BackColor);
+                    myPen.Width = trackBar1.Value;
+                    if (flagEcl == true)
+                        my_g.DrawEllipse(myPen, startPoint.X, startPoint.Y, e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
+
+                    myPen.Dispose();
+                }
+            }
+            else if(line == true)
+            {
+                Pen myPen = new Pen(panel_color.BackColor);
+                myPen.Width = trackBar1.Value;
+                my_g.DrawLine(myPen, startPoint.X, startPoint.Y, e.X, e.Y);
+                myPen.Dispose();
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (flagRec || flagEcl || line) return;
+            if (isDrawing == true)
+            {
+                my_g.FillEllipse(mybrush, e.X, e.Y, trackBar1.Value, trackBar1.Value);
+            }
+          
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            flagRec = flagEcl = line = false;
+            mybrush.Color = panel1.BackColor;
+            if (isDrawing == true)
+            {
+                my_g.FillEllipse(mybrush, e.X, e.Y, trackBar1.Value, trackBar1.Value);
+            }
+            
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            flagRec = flagEcl = line = false;
+            mybrush.Color = colorDialog1.Color;
+            if (isDrawing == true)
+            {
+                my_g.FillEllipse(mybrush, e.X, e.Y, trackBar1.Value, trackBar1.Value);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (line == true) line = false;
+            else if(line == false) line = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            flagRec = false;
+            line = false;
+            if (flagEcl == true) flagEcl = false;
+            else if (flagEcl == false) flagEcl = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            flagEcl = false;
+            line = false;
+            if(flagRec == true) flagRec = false;
+            else if(flagRec == false) flagRec = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            flagRec = false;
+            flagEcl = false;
+            line = false;
+            if (filling == true) filling = false;
+            else if(filling == false) filling = true;
+        }
+    }
+}
